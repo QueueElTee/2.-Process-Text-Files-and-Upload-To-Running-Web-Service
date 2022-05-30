8e4309e5eca407b893190fc07dc89d55d76ba916
QueueElTee <tijaniqueen17@gmail.com>
2022-05-30 17:16:55 +0100

    Add web service project

---
+++ README.md
@@ -0,0 +1,10 @@
+# Process Text Files with Python Dictionaries and Upload To a Running Web Service
+My solution to the second project of the Automating Real World Tasks With Python course (Course 6 of the IT Automation with Python Specialization)
+
+---
+
+## What it does
+
+- Uses the python OS module to process a directory of text files
+- Manages information stored inn python dictonaries
+- Uses the python requests module to upload content to a running web service
---
+++ project_solution.py
@@ -0,0 +1,40 @@
+import os
+import requests
+
+directory = 'feedback'
+txt_files = os.listdir(directory)
+
+list_of_reviews = []
+list_of_reviews_dict = []
+
+
+def process_txt_files():
+    for file in txt_files:
+        full_path = os.path.abspath(directory + '/' + file)
+        with open(full_path, 'r') as review:
+            single_review_contents = []
+            for line in review.readlines():
+                single_review_contents.append(line.strip())
+        list_of_reviews.append(single_review_contents)
+            
+
+def generate_dictionaries():
+    for review in list_of_reviews:
+        review_dict = {}
+        review_dict['title'] = review[0]
+        review_dict['name'] = review[1]
+        review_dict['date'] = review[2]
+        review_dict['feedback'] = review[3]
+        list_of_reviews_dict.append(review_dict)
+        
+        
+def post_to_web_service():
+    url = 'web-service-url-here'
+    for review in list_of_reviews_dict:
+        r = requests.post(url, data=review)
+        r.raise_for_status()
+        
+        
+process_txt_files()
+generate_dictionaries()
+# post_to_web_service()
